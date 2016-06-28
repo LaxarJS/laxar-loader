7d0ee7180a31682570bcb69ba9a09bfac8f59c5b
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-06-28 13:15:49 +0200

    refactor, allow both file read and module load to get deps

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var fs = require( 'fs' );
 var path = require( 'path' ).posix;
 
 var loaderUtils = require( 'loader-utils' );
@@ -8,8 +7,9 @@ var laxarTooling = require( 'laxar-tooling' );
 
 module.exports = function( source ) {
    var loaderContext = this;
-   var query = loaderUtils.parseQuery( this.query );
    var done = this.async();
+   var success = module => done( null, module );
+   var query = loaderUtils.parseQuery( this.query );
 
    var logger = {
       error: this.emitError
@@ -19,41 +19,47 @@ module.exports = function( source ) {
       this.cacheable();
    }
 
-   var flowPath = loaderContext.resourcePath;
+   var readJson = moduleReader( this, query[ 'json-loader' ] ) ||
+                  traceDependencies( this, laxarTooling.jsonReader.create( logger ) );
+
+   var readFile = moduleReader( this, query[ 'file-loader' ] ) ||
+                  traceDependencies( this, laxarTooling.fileReader.create( logger ) );
+
+   var artifactCollector = laxarTooling.artifactCollector.create( logger, {
+      projectPath,
+      readJson
+   } );
+
+   var artifactsPromise = projectPath( this.resourcePath )
+         .then( flowPath => [ flowPath ] )
+         .then( artifactCollector.collectArtifacts )
 
    if( query.artifacts ) {
-      var artifactCollector = laxarTooling.artifactCollector.create( logger, {
-         projectPath: projectPath,
-         readJson: readJson
-      } );
-
-      projectPath( loaderContext.resourcePath )
-         .then( function( flowPath ) {
-            return artifactCollector.collectArtifacts( [ flowPath ] );
-         } )
-         .then( exportObject, done );
+      artifactsPromise
+         .then( exportObject )
+         .then( success, done );
    }
 
    if( query.resources ) {
       var resourceCollector = laxarTooling.resourceCollector.create( logger, {
-         readFile: readFile,
+         readFile,
          embed: !!(query.embed)
       } );
 
-      projectPath( loaderContext.resourcePath )
-         .then( loadArtifacts )
+      artifactsPromise
          .then( resourceCollector.collectResources )
-         .then( exportObject, done );
+         .then( exportObject )
+         .then( success, done );
    }
 
    if( query.dependencies ) {
       var dependencyCollector = laxarTooling.dependencyCollector.create( logger, {
       } );
 
-      projectPath( loaderContext.resourcePath )
-         .then( loadArtifacts )
+      artifactsPromise
          .then( dependencyCollector.collectDependencies )
-         .then( exportDependencies, done );
+         .then( exportDependencies )
+         .then( success, done );
    }
 
    function projectPath( ref ) {
@@ -64,94 +70,74 @@ module.exports = function( source ) {
                // to resolve a directory, we replace all aliases.
                filename = resolveAliases( ref, loaderContext.options.resolve.alias );
             }
-            resolve( relPath( filename ) );
+            resolve( path.relative( loaderContext.options.context || '', filename ) );
          } );
       } );
    }
-
-   function loadArtifacts( flowPath ) {
-      return new Promise( function( resolve, reject ) {
-         var filename = absPath( flowPath );
-         loaderContext.loadModule( 'laxar-loader?artifacts!' + filename, function( err, data ) {
-            if( err ) {
-               return reject( err );
-            }
-            return resolve( loaderContext.exec( data, filename ) );
-         } );
-      } );
-   }
-
-
-   function readJson( ref ) {
-      var filename = absPath( ref );
-      return new Promise( function( resolve, reject ) {
-         if( filename === flowPath ) {
-            return resolve( loaderContext.exec( source, filename ) );
-         }
-
-         loaderContext.addDependency( filename );
-         loaderContext.loadModule( filename, function( err, data ) {
-            if( err ) {
-               return reject( err );
-            }
-
-            resolve( loaderContext.exec( data, filename ) );
-         } );
-      } );
-   }
-
-   function readFile( ref ) {
-      var filename = absPath( ref );
-      return new Promise( function( resolve, reject ) {
-         loaderContext.addDependency( filename );
-         fs.readFile( filename, 'utf-8', function( err, data ) {
-            if( err ) {
-               return reject( err );
-            }
-
-            resolve( data );
-         } );
-      } );
-   }
-
-   function exportObject( object ) {
-      done( null, 'module.exports = ' + JSON.stringify( object, undefined, "\t" ) );
-   }
-
-   function exportDependencies( modulesByTechnology ) {
-      var dependencies = [];
-      var registryEntries = [];
-
-      Object.keys( modulesByTechnology )
-         .reduce( function( start, technology ) {
-            var end = start + modulesByTechnology[ technology].length;
-            [].push.apply( dependencies, modulesByTechnology[ technology ] );
-            registryEntries.push( '\'' + technology + '\': modules.slice( ' + start + ', ' + end + ' )' );
-            return end;
-         }, 0 );
-
-      var requireString = '[\n   require( \'' + dependencies.join( '\' ),\n   require( \'' ) + '\' )\n]';
-
-      done( null, 'var modules = ' + requireString + ';\n' +
-                  'module.exports = {\n' +
-                  '   ' + registryEntries.join( ',\n   ' ) + '\n' +
-                  '};\n' );
-
-   }
-
-   function resolveAliases( string, aliases ) {
-      return Object.keys( aliases ).reduce( function( string, alias ) {
-         var pattern = new RegExp( '(^|/)' + alias + '($|/)', 'g' );
-         return string.replace( pattern, '$1' + aliases[ alias ] + '$2' );
-      }, string );
-   }
-
-   function relPath( ref ) {
-      return path.relative( loaderContext.options.context || '', ref );
-   }
-
-   function absPath( ref ) {
-      return path.resolve( loaderContext.options.context || '', ref );
-   }
-
 };
+
+function moduleReader( loaderContext, loader ) {
+   if( !loader ) {
+      return;
+   }
+
+   return function readModule( ref ) {
+      var filename = path.resolve( loaderContext.options.context || '', ref );
+      var request = isString( loader ) ? '-!' + loader + '!' + filename : filename;
+
+      return new Promise( function( resolve, reject ) {
+         loaderContext.addDependency( filename );
+         loaderContext.loadModule( request, function( err, module ) {
+            if( err ) {
+               reject( err );
+            } else {
+               resolve( loaderContext.exec( module, filename ) );
+            }
+         } );
+      } );
+   };
+}
+
+function isString( something ) {
+   return ( typeof something === 'string' ) || ( something instanceof String );
+}
+
+function traceDependencies( loaderContext, fn ) {
+   return function( ref, ...args ) {
+      var filename = path.resolve( loaderContext.options.context || '', ref );
+      loaderContext.addDependency( filename );
+      return fn( filename, ...args );
+   };
+}
+
+function resolveAliases( string, aliases ) {
+   return Object.keys( aliases ).reduce( function( string, alias ) {
+      var pattern = new RegExp( '(^|/)' + alias + '($|/)', 'g' );
+      return string.replace( pattern, '$1' + aliases[ alias ] + '$2' );
+   }, string );
+}
+
+function exportObject( object ) {
+   return 'module.exports = ' + JSON.stringify( object, undefined, "\t" ) + ';';
+}
+
+function exportDependencies( modulesByTechnology ) {
+   var dependencies = [];
+   var registryEntries = [];
+
+   Object.keys( modulesByTechnology )
+      .reduce( function( start, technology ) {
+         var end = start + modulesByTechnology[ technology ].length;
+         [].push.apply( dependencies, modulesByTechnology[ technology ] );
+         registryEntries.push( '\'' + technology + '\': modules.slice( ' + start + ', ' + end + ' )' );
+         return end;
+      }, 0 );
+
+   var requireString = '[\n   require( \'' + dependencies.join( '\' ),\n   require( \'' ) + '\' )\n]';
+
+   return 'var modules = ' + requireString + ';\n' +
+          'module.exports = {\n' +
+          '   ' + registryEntries.join( ',\n   ' ) + '\n' +
+          '};\n';
+}
+
