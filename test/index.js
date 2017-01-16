672264964f6e538b2decc4ef78daec66f5545868
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-01-16 16:57:58 +0100

    (#22) fix tests for webpack 2

--- test/index.js
+++ test/index.js
@@ -2,83 +2,86 @@ import vm from 'vm';
 import path from 'path';
 import webpack from 'webpack';
 import defaults from './webpack.config.js';
+import { MockFSPlugin } from './mockfs';
+
 import { expect } from 'chai';
 
 describe( 'laxar-loader', () => {
 
    it( 'does not explode', done => {
 
+      const mockfs = new MockFSPlugin( {
+         'laxar.config.js': 'module.exports = ' + JSON.stringify( {
+            paths: {
+               flows: 'app/flows',
+               pages: 'app/pages',
+               themes: 'components/themes',
+               layouts: 'components/layouts',
+               widgets: 'components/widgets',
+               controls: 'components/controls',
+               schemas: 'schemas',
+               'default-theme': 'default.theme'
+            }
+         } ) + ';',
+         'init.js': 'module.exports = require( \'laxar-loader?flow=main!./dummy.js\' );',
+         'dummy.js': '/* empty */',
+         'app/': {
+            'flows/main.json': '{ "places": { "entry": { "page": "test" } } }',
+            'pages/test.json': '{ "layout": "main", "areas": { "main": [ { "widget": "test-widget" } ] } }'
+         },
+         'components/themes/default.theme/': {
+            'css/theme.css': '/* empty */'
+         },
+         'components/layouts/main/': {
+            'layout.json': '{ "name": "main" }',
+            'css/main.css': '/* empty */'
+         },
+         'components/widgets/test-widget/': {
+            'widget.json': '{ "name": "test-widget", "controls": [ "test-control" ] }',
+            'test-widget.js': 'module.exports = function TestWidget() {};',
+            'default.theme': {
+               'test-widget.html': '<blink>test-widget</blink>',
+               'css/test-widget.css': 'blink { color: deeppink; }'
+            }
+         },
+         'components/controls/test-control/': {
+            'control.json': '{ "name": "test-control" }',
+            'test-control.js': 'module.exports = function TestControl() {};'
+         },
+         'schemas/': {
+            'flow.json': JSON.stringify( {
+               $schema: 'http://json-schema.org/draft-04/schema#',
+               type: 'object',
+               required: [ 'places' ]
+            } ),
+            'theme.json': JSON.stringify( {
+               $schema: 'http://json-schema.org/draft-04/schema#',
+               type: 'object'
+            } ),
+            'page.json': JSON.stringify( {
+               $schema: 'http://json-schema.org/draft-04/schema#',
+               type: 'object'
+            } ),
+            'layout.json': JSON.stringify( {
+               $schema: 'http://json-schema.org/draft-04/schema#',
+               type: 'object'
+            } ),
+            'widget.json': JSON.stringify( {
+               $schema: 'http://json-schema.org/draft-04/schema#',
+               type: 'object',
+               required: [ 'name' ]
+            } ),
+            'control.json': JSON.stringify( {
+               $schema: 'http://json-schema.org/draft-04/schema#',
+               type: 'object',
+               required: [ 'name' ]
+            } )
+         }
+      } );
       const compiler = webpack( {
          ...defaults,
          entry: './init.js',
-         mock: {
-            'laxar.config.json': 'module.exports = ' + JSON.stringify( {
-               paths: {
-                  flows: 'app/flows',
-                  pages: 'app/pages',
-                  themes: 'components/themes',
-                  layouts: 'components/layouts',
-                  widgets: 'components/widgets',
-                  controls: 'components/controls',
-                  schemas: 'schemas',
-                  'default-theme': 'default.theme'
-               }
-            } ) + ';',
-            'init.js': 'module.exports = require( \'laxar-loader?flow=main!dummy.js\' );',
-            'dummy.js': '/* empty */',
-            'app/': {
-               'flows/main.json': '{ "places": { "entry": { "page": "test" } } }',
-               'pages/test.json': '{ "layout": "main", "areas": { "main": [ { "widget": "test-widget" } ] } }'
-            },
-            'components/themes/default.theme/': {
-               'css/theme.css': '/* empty */'
-            },
-            'components/layouts/main/': {
-               'layout.json': '{ "name": "main" }',
-               'css/main.css': '/* empty */'
-            },
-            'components/widgets/test-widget/': {
-               'widget.json': '{ "name": "test-widget", "controls": [ "test-control" ] }',
-               'test-widget.js': 'module.exports = function TestWidget() {};',
-               'default.theme': {
-                  'test-widget.html': '<blink>test-widget</blink>',
-                  'css/test-widget.css': 'blink { color: deeppink; }'
-               }
-            },
-            'components/controls/test-control/': {
-               'control.json': '{ "name": "test-control" }',
-               'test-control.js': 'module.exports = function TestControl() {};'
-            },
-            'schemas/': {
-               'flow.json': JSON.stringify( {
-                  $schema: 'http://json-schema.org/draft-04/schema#',
-                  type: 'object',
-                  required: [ 'places' ]
-               } ),
-               'theme.json': JSON.stringify( {
-                  $schema: 'http://json-schema.org/draft-04/schema#',
-                  type: 'object'
-               } ),
-               'page.json': JSON.stringify( {
-                  $schema: 'http://json-schema.org/draft-04/schema#',
-                  type: 'object'
-               } ),
-               'layout.json': JSON.stringify( {
-                  $schema: 'http://json-schema.org/draft-04/schema#',
-                  type: 'object'
-               } ),
-               'widget.json': JSON.stringify( {
-                  $schema: 'http://json-schema.org/draft-04/schema#',
-                  type: 'object',
-                  required: [ 'name' ]
-               } ),
-               'control.json': JSON.stringify( {
-                  $schema: 'http://json-schema.org/draft-04/schema#',
-                  type: 'object',
-                  required: [ 'name' ]
-               } )
-            }
-         }
+         plugins: [ mockfs ]
       } );
 
       compileAndRun( compiler, ( err, result ) => {
